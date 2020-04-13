858b29971c6abbe705168c37588fccdaff9f07bc
Andrew Ware <aceware152dev@gmail.com>
2020-04-13 21:06:45 +1000

    adding walking animation

--- Castra Tumultus/Assets/Game/Scripts/RigidbodyCharacter.cs
+++ Castra Tumultus/Assets/Game/Scripts/RigidbodyCharacter.cs
@@ -15,9 +15,13 @@ public class RigidbodyCharacter : MonoBehaviour
     private Vector3 _inputs = Vector3.zero;
     private bool _isGrounded = true;
     private Transform _groundChecker;
+    private bool walking;
+    private Animator _anim;
 
     void Start()
     {
+
+        _anim = GetComponent<Animator>();
         _body = GetComponent<Rigidbody>();
         _groundChecker = transform.GetChild(0);
     }
@@ -30,6 +34,21 @@ public class RigidbodyCharacter : MonoBehaviour
         _inputs = Vector3.zero;
         _inputs.x = Input.GetAxis("Horizontal");
         _inputs.z = Input.GetAxis("Vertical");
+
+        //walking andimation
+
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")
+            || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow)
+            || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            walking = true;
+
+        }
+
+        else walking = false;
+
+        _anim.SetBool("Walk", walking);
+
         if (_inputs != Vector3.zero)
             transform.forward = _inputs;
 
@@ -42,6 +61,10 @@ public class RigidbodyCharacter : MonoBehaviour
             Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
             _body.AddForce(dashVelocity, ForceMode.VelocityChange);
         }
+
+
+
+
     }
 
 
