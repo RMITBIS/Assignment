5b4242ff0f884739ab26c5cd2a152b19b7760e61
lclc98 <lclc98@lclc98.com>
2020-05-12 17:19:13 +1000

    Added rotation based on mouse Fixed keyboard affecting mouse movements

--- Castra Tumultus/Assets/Game/Scripts/RigidbodyCharacter.cs
+++ Castra Tumultus/Assets/Game/Scripts/RigidbodyCharacter.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class RigidbodyCharacter : MonoBehaviour
 {
-
     public float Speed = 5f;
     public float JumpHeight = 2f;
     public float GroundDistance = 0.2f;
@@ -20,7 +18,6 @@ public class RigidbodyCharacter : MonoBehaviour
 
     void Start()
     {
-
         _anim = GetComponent<Animator>();
         _body = GetComponent<Rigidbody>();
         _groundChecker = transform.GetChild(0);
@@ -28,8 +25,15 @@ public class RigidbodyCharacter : MonoBehaviour
 
     void Update()
     {
-        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        float h = Input.mousePosition.x - Screen.width / 2f;
+        float v = Input.mousePosition.y - Screen.height / 2f;
 
+        float yRotation = (float) Math.Atan2(h, v);
+        Quaternion target = Quaternion.Euler(0, yRotation * 60f, 0);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 10f);
+
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
 
         _inputs = Vector3.zero;
         _inputs.x = Input.GetAxis("Horizontal");
@@ -42,7 +46,6 @@ public class RigidbodyCharacter : MonoBehaviour
             || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
             walking = true;
-
         }
 
         else walking = false;
@@ -50,21 +53,20 @@ public class RigidbodyCharacter : MonoBehaviour
         _anim.SetBool("Walk", walking);
 
         if (_inputs != Vector3.zero)
-            transform.forward = _inputs;
+            transform.TransformVector(_inputs);
 
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
             _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
         }
-        if (Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
-            _body.AddForce(dashVelocity, ForceMode.VelocityChange);
-        }
 
+        // Was causing an error in console
 
-
-
+        // if (Input.GetButtonDown("Dash"))
+        // {
+        //     Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
+        //     _body.AddForce(dashVelocity, ForceMode.VelocityChange);
+        // }
     }
 
 
